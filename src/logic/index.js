c0a331a86a782ba9e4cd836a3bdba9abbe15ca65
dfoxpro <dfoxpro@gmail.com>
2019-02-06 23:05:31 -0500

    remove test code

--- src/logic/index.js
+++ src/logic/index.js
@@ -1,31 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {PlaylistEl} from '../view/jsx_components/playlist_element.jsx'
 
-import Media from '../view/jsx_components/media.jsx'
-import Asd from '../view/jsx_components/asd.jsx'
-// import {B} from './render_md.js'
-// console.log('hello world');
-// B.zxc();
-// (new B).qwe;
+import 'babel-polyfill'
 
-function reactTest() {
-	function ReactTagTest() {
-		return (
-			<b className = "react_tag_test">
-				<span>C'est un élément généré par JSX</span>
-			</b>
-		);
-	}
+// import Media from '../view/jsx_components/media.jsx'
+// import mediaImg from '../assets/images/cover.jpg'
+
+async function reactTest() {
 	const render = (reactElement, hook_element = 'react_hook') => {
 		ReactDOM.render(
 			reactElement,
 			document.getElementById(hook_element)
 		);
 	}
-	// const {Media} = await import('../view/jsx_components/media.jsx');
-	// console.log(Media);
-	// render(<ReactTagTest />);
-	render(<Asd />, 'asd');
-	render(<Media/>);
+
+	// const {Playlist} = await import('../view/jsx_components/playlist.jsx');
+	const apiIndex = await import('../data_fixtures/api_index.json');
+	// const mediaImg = images['cover.jpg']
+	// console.log('mediaImg', mediaImg, typeof(mediaImg));
+	console.log('apiIndex', apiIndex, typeof(apiIndex));
+
+	render(pug`
+		PlaylistEl(...apiIndex)
+	`);
 };
 reactTest();
